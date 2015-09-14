1fbfa5716971cc0e5dcef1b861c46bbec637ef96
Behram ÇELEN <behramcelen@gmail.com>
2015-09-15 01:26:37 +0300

    create user skills relation

--- Entity/Skill.php
+++ Entity/Skill.php
@@ -2,7 +2,9 @@
 
 namespace OkulBilisim\EndorsementBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use OkulBilisim\EndorsementBundle\Entity\Traits\GenericEntityTrait;
+use Ojs\UserBundle\Entity\User;
 
 /**
  * Class Skill
@@ -23,10 +25,16 @@ class Skill
     private $name;
 
     /**
+     * @var ArrayCollection|User[]
+     */
+    private $skillUsers;
+
+    /**
      * Skill constructor.
      */
     public function __construct()
     {
+        $this->skillUsers = new ArrayCollection();
     }
 
     /**
@@ -54,6 +62,36 @@ class Skill
     public function setName($name)
     {
         $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return ArrayCollection|User[]
+     */
+    public function getSkillUsers()
+    {
+        return $this->skillUsers;
+    }
+
+    /**
+     * @param ArrayCollection|User[] $skillUsers
+     * @return $this
+     */
+    public function setSkillUsers($skillUsers)
+    {
+        $this->skillUsers = $skillUsers;
+        return $this;
+    }
+
+    /**
+     * @param  User $user
+     * @return Skill
+     */
+    public function addSkillUser(User $user)
+    {
+        if (!$this->skillUsers->contains($user)) {
+            $this->skillUsers->add($user);
+        }
         return $this;
     }
 }
